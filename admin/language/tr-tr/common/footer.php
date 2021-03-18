b0297ef663372d5364690fc4978498a0385bba3b
Emrah Atalay <emrahatalay93@gmail.com>
2021-03-18 23:34:52 +0300

    first commit

---
+++ admin/language/tr-tr/common/footer.php
@@ -0,0 +1,5 @@
+<?php
+
+// Text
+$_['text_footer']  = 'Mupity.com Copyright &copy; ' . date('Y') . ' Tüm Hakları Saklıdır.';
+$_['text_version'] = 'Sürüm %s';
---
+++ catalog/language/en-gb/common/cart.php
@@ -0,0 +1,7 @@
+<?php
+// Text
+$_['text_items']     = '<h3 class="hidden-xs">My Cart</h3> <p> %s </p> <span>Items</span>';
+$_['text_empty']     = 'Your shopping cart is empty!';
+$_['text_cart']      = 'View Cart';
+$_['text_checkout']  = 'Checkout';
+$_['text_recurring'] = 'Payment Profile';
