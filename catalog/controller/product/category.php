776aee2c7188861f97162febda79fb564ae1b026
Emrah Atalay <eatalay@re-os.com>
2021-10-13 17:47:09 +0300

    Kategoriler sayfalama adetleri değiştirildi.

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -328,7 +328,7 @@ class ControllerProductCategory extends Controller {
 
 			$data['limits'] = array();
 
-			$limits = array_unique(array($this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit'), 25, 50, 75, 100));
+			$limits = array_unique(array($this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit'), 24, 52, 76, 100));
 
 			sort($limits);
 
