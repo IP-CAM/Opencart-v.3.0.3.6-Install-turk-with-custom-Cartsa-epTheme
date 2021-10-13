0b2e35236280ed27c43fc20012374eed9e0644a7
Emrah Atalay <eatalay@re-os.com>
2021-10-13 22:02:13 +0300

    az kaldı

--- admin/model/catalog/product.php
+++ admin/model/catalog/product.php
@@ -731,7 +731,7 @@ class ModelCatalogProduct extends Model {
 		
 		$row = 0;
 		foreach ($categories as &$category_id) {
-		    for ($colors[$row] as &$color) {
+		    foreach ($colors[$row] as &$color) {
 		        $sql = "INSERT INTO oc_product_option_value (product_option_id, product_id, option_id, option_value_id, quantity, subtract, price, price_prefix, points, points_prefix, weight, weight_prefix) SELECT product_option_id, product_id, option_id, ". $color . ", 0, 0, 0, '+', 0, '+', 0, '+' FROM oc_product_option WHERE product_id IN(SELECT DISTINCT product_id FROM oc_product_to_category WHERE category_id LIKE ". $category_id ." AND option_id LIKE 48);";
 
 				$query = $this->db->query($sql);
